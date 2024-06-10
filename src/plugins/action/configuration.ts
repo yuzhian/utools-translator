6a8f4bebbaf001f538ea633569912fedca19b584
yuzhian <yu97@live.com>
2024-06-10 16:04:11 +0800

    feat: 将默认快捷键更改为更符合直觉的方式

--- src/plugins/action/configuration.ts
+++ src/plugins/action/configuration.ts
@@ -11,37 +11,37 @@ export const allActions = [
     key: "srcLang",
     text: "当前语种",
     actions: [
-      { key: "srcLangPrev", text: "上一个语种", preset: "Alt+q" },
-      { key: "srcLangNext", text: "下一个语种", preset: "Alt+e" }
+      { key: "srcLangPrev", text: "上一个语种", preset: "Control+ArrowLeft" },
+      { key: "srcLangNext", text: "下一个语种", preset: "Control+ArrowRight" }
     ]
   },
   {
     key: "dstLang",
     text: "目标语种",
     actions: [
-      { key: "dstLangPrev", text: "上一个语种", preset: "Control+q" },
-      { key: "dstLangNext", text: "下一个语种", preset: "Control+e" },
+      { key: "dstLangPrev", text: "上一个语种", preset: "Alt+ArrowLeft" },
+      { key: "dstLangNext", text: "下一个语种", preset: "Alt+ArrowRight" },
     ]
   },
   {
     key: "srcText",
     text: "原文",
     actions: [
-      { key: "srcInputFocus", text: "定位输入框", preset: "Control+Space" },
-      { key: "srcTextFromClipboard", text: "读取剪切板到输入框", preset: "Control+Shift+v" },
-      { key: "srcTextSentenceCase", text: "转为语句形式并翻译", preset: "Alt+1" },
-      { key: "srcTextExtractComment", text: "提取注释并翻译", preset: "Alt+2" }
+      { key: "srcInputFocus", text: "将光标置于输入框", preset: "Control+Shift+i" },
+      { key: "srcTextFromClipboard", text: "将剪贴板内容粘贴到输入框", preset: "Control+Shift+v" },
+      { key: "srcTextSentenceCase", text: "拆解变量文本并组成句子", preset: "Control+Shift+s" },
+      { key: "srcTextExtractComment", text: "提取注释中的文本并翻译", preset: "Control+Shift+e" }
     ]
   },
   {
     key: "dstText",
     text: "译文",
     actions: [
-      { key: "dstTextCamelCaseCopy", text: "以驼峰形式复制", preset: "Control+1" },
-      { key: "dstTextPascalCaseCopy", text: "以大驼峰形式复制", preset: "Control+2" },
-      { key: "dstTextSnakeCaseCopy", text: "以蛇形复制", preset: "Control+3" },
-      { key: "dstTextScreamingSnakeCaseCopy", text: "以大蛇形复制", preset: "Control+4" },
-      { key: "dstTextKebabCaseCopy", text: "以连字符形式复制", preset: "Control+5" }
+      { key: "dstTextCamelCaseCopy", text: "以驼峰形式复制", preset: "Control+Shift+1" },
+      { key: "dstTextPascalCaseCopy", text: "以大驼峰形式复制", preset: "Control+Shift+2" },
+      { key: "dstTextSnakeCaseCopy", text: "以蛇形复制", preset: "Control+Shift+3" },
+      { key: "dstTextScreamingSnakeCaseCopy", text: "以大蛇形复制", preset: "Control+Shift+4" },
+      { key: "dstTextKebabCaseCopy", text: "以连字符形式复制", preset: "Control+Shift+5" }
     ]
   }
 ]
