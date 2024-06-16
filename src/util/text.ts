35ec01471d268ace97c52faedeeacdc2716680f0
yuzhian <yu97@live.com>
2024-06-16 15:00:29 +0800

    refactor: 优化提取注释函数

--- src/util/text.ts
+++ src/util/text.ts
@@ -1,22 +1,78 @@
+const COMMENT_HANDLERS = [
+  // ------------ 文档注释 ------------------------
+  // /** */: C/C++/C#/Java/JavaScript
+  {
+    regex: /\/\*\*[\s\S]*?\*\//g,
+    handler: (comment: string) => mergeLinesWithoutPunctuation(comment.replace(/^\/\*\*|\*\/$/g, '').replace(/^\s*\*\s?/gm, '').trim()),
+  },
+  // ------------ 多行注释 ------------------------
+  // /* */: C/C++/C#/Java/JavaScript
+  {
+    regex: /\/\*[\s\S]*?\*\//g,
+    handler: (comment: string) => mergeLinesWithoutPunctuation(comment.replace(/^\/\*|\*\/$/g, '').replace(/^\s*\*\s?/gm, '').trim()),
+  },
+  // """ """: Python
+  {
+    regex: /""".*?"""/gs,
+    handler: (comment: string) => mergeLinesWithoutPunctuation(comment.replace(/^"""/gm, '').replace(/"""\s*$/gm, '').trim()),
+  },
+  // {- -}: Haskell
+  {
+    regex: /{-[\s\S]*?-}/g,
+    handler: (comment: string) => mergeLinesWithoutPunctuation(comment.replace(/{-\s*/g, '').replace(/\s*-}/g, '').trim()),
+  },
+  // =begin =end: Ruby
+  {
+    regex: /=begin[\s\S]*?=end/g,
+    handler: (comment: string) => mergeLinesWithoutPunctuation(comment.replace(/^=begin/gm, '').replace(/=end\s*$/gm, '').trim()),
+  },
+  // ------------ 单行注释 ------------------------
+  // //: C/C++/C#/Java/JavaScript
+  {
+    regex: /\/\/.*$/gm,
+    handler: (comment: string) => comment.replace(/^\/\/\s?/gm, '').trim(),
+  },
+  // --: SQL
+  {
+    regex: /--.*$/gm,
+    handler: (comment: string) => comment.replace(/^--\s?/gm, '').trim(),
+  },
+  // #: Bash/Shell/Python/Ruby
+  {
+    regex: /#.*$/gm,
+    handler: (comment: string) => comment.replace(/^#\s?/gm, '').trim(),
+  },
+  // %: MATLAB
+  {
+    regex: /%.*$/gm,
+    handler: (comment: string) => comment.replace(/^%\s?/gm, '').trim(),
+  },
+]
+
+/**
+ * 合并无符号的换行
+ */
+const mergeLinesWithoutPunctuation = (text: string) => {
+  const lines = text.trim().split('\n')
+  for (let i = 0; i < lines.length - 1;) {
+    const currentLine = lines[i].trim()
+    const nextLine = lines[i + 1].trim()
+    if (/[.,;:!?，。；：！？、（）【】《》“”‘’]/.test(currentLine.slice(-1))) {
+      i++
+      continue
+    }
+    lines[i] = currentLine + ' ' + nextLine
+    lines.splice(i + 1, 1)
+  }
+  return lines.join('\n')
+}
+
 /**
  * 提取注释内容
+ * @param {string} text 文本
  */
-export const extractComment = (text: string) => {
-  return text
-    // 去除多行注释头
-    .replace(/\/\*+/g, "")
-    // 去除多行注释尾
-    .replace(/\*\//g, "")
-    // 当前行 @ 开头, 则换行
-    .replace(/\n[ \t]*\*[ \t]*(@)/g, "\n$1")
-    // 上一行末尾是文本, 则拼接
-    .replace(/([\u4e00-\u9fa5a-zA-Z0-9])[ \t]*\n[ \t]*\*[ \t]*/g, "$1 ")
-    // 上一行末尾是符号, 则换行
-    .replace(/([^\u4e00-\u9fa5a-zA-Z0-9])[ \t]*\n[ \t]*\*[ \t]*/g, "$1\n")
-    // 去除行首 *
-    .replace(/\n[ \t]*\*[ \t]*/g, "\n")
-    // 去除标签
-    .replace(/<[^>]+>/g, "")
-    // 去除多余的空白符号
-    .replace(/[ \t]+/g, " ")
-}
+export const extractComment = (text: string) =>
+  COMMENT_HANDLERS.reduce((processedText, { regex, handler }) =>
+      processedText.replace(regex, handler)
+    , text
+  )
